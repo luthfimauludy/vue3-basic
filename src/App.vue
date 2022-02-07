b15a02e0543b4ff8292e941dde2038e021895226
Luthfi Putra Mauludy <putra.luthfi@gmail.com>
2022-02-07 21:56:13 +0700

    menambahkan message hi world

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
-    <div>message</div>
+    <div>{{message}}</div>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            message: 'hi world'
+        }
+    }
 };
 </script>
