490287a6706944ea1e5b1d1c12541e6d5f84ab77
Luthfi Putra Mauludy <putra.luthfi@gmail.com>
2022-03-05 11:15:21 +0700

    menambahkan pesan error

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,15 @@
     <div class="container" style="margin-top: 20px">
         <div class="card">
             <div class="card-body">
-                <h5 class="card-title">SIMPLE TODO APP</h5>
+                <h5 class="card-title">SIMPLE TODOLIST</h5>
                 <div class="row">
                     <div class="col-10">
                         <input type="text" class="form-control" v-model="todo" placeholder="Ketik disini" @keyup.enter="add"/>
+                        <div v-if="errors" class="error-messages">
+                            <div v-for="(error, index) in errors" :key="index">
+                                <small>{{ error }}</small>
+                            </div>
+                        </div>
                     </div>
                     <div class="col-2">
                         <button class="btn btn-success" @click="add">ADD</button>
@@ -31,7 +36,8 @@ export default {
     data() {
         return {
             todo: "",
-            todos: []
+            todos: [],
+            errors: []
         };
     },
     beforeMount() {
@@ -46,9 +52,13 @@ export default {
     },
     methods: {
         add() {
-            if (this.todo === '') {
+            this.errors = [];
+
+            if (!this.todo) {
+                this.errors.push("Data tidak boleh kosong!");
                 return false;
             }
+
             this.todos.unshift({
                 activity: this.todo,
                 isDone: false
